ee85ed7ff1af44bcd0d024400860f0830eae6266
Martin Møller <mmj@dbc.dk>
2012-04-02 16:22:16 +0200

    final changes

--- bibdkcaptcha.js
+++ bibdkcaptcha.js
@@ -2,33 +2,36 @@
     Drupal.behaviors.bibdkcaptcha = {
         attach : function(context) {
             $("#refreshbtn").click(function() {
-                $("#captcha img:first-child").fadeOut(200, function(){
-                    $("#captcha img:first-child").remove();
-                });
                 jQuery.ajax({
                     type: 'GET',
                     url:'create_login/refreshcatptcha',
                     success: updateCaptcha,
                     dataType: JSON
                 });
-            });
-            
+            });            
             $("#audiobtn").click(function() {
                 playCaptcha();
+                $("input#edit-captcha-response").focus();
             });
-            
             function playCaptcha(){
-                var type = 'mp3';
-                //console.log($.browser['safari']);
+                var type = 'mp3'; 
                 if($.browser['safari']){
                     type = 'wav';
+                    $('#playcaptcha').attr("type","audio/x-wav");
                 }
-                var url = 'create_login/playaudiocaptcha'+"/"+$('input[name=captcha_sid]').val()+"/"+$('input[name=captcha_token]').val()+"/"+type;
-                $('#playcaptcha').attr("src",url);
+                var url = 'create_login/playaudiocaptcha'+"/"+$('input[name=captcha_sid]').val()+"/"+$('input[name=captcha_token]').val()+"/"+type;   
+                if($.browser['msie']){
+                    if($('#playcaptcha')){
+                        $('#playcaptcha').remove();
+                    }
+                    $('#captcha').append('﻿<embed id="embedobj" hidden="true" src="' + url +'" ></embed>');
+                } else {
+                    $('#playcaptcha').attr("src",url);
+                }
             }
-            
             function updateCaptcha(data){
                 var data = jQuery.parseJSON(data);
+                $("#captcha img:first-child").remove();
                 $(data['captcha']).prependTo("#captcha");
                 $('input[name=captcha_sid]').val(data['sid']);
                 $('input[name=captcha_token]').val(data['token']);
