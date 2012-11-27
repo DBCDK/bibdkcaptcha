ca8c2f8aecac2d478838edb56537f5a5d6f6c7a9
Martin Møller <mmj@dbc.dk>
2012-11-27 09:58:59 +0100

    Cleanup

--- js/bibdkcaptcha.js
+++ js/bibdkcaptcha.js
@@ -1,29 +1,29 @@
-(function($) {
+(function ($) {
     Drupal.behaviors.bibdkcaptcha = {
-        attach : function(context) {
+        attach:function (context) {
             var host = "http://" + document.location.hostname;
-            
+
             var basePath = Drupal.settings.basePath;
-            if(basePath.length){
+            if (basePath.length) {
                 host = host + basePath;
             }
 
-            $("#bibdkcaptcha-controls-refreshbtn").click(function() {
+            $("#bibdkcaptcha-controls-refreshbtn").click(function () {
                 jQuery.ajax({
-                    type: 'GET',
+                    type:'GET',
                     url:host + 'captcha/refreshcatptcha',
-                    success: updateCaptcha,
-                    dataType: JSON
+                    success:updateCaptcha,
+                    dataType:JSON
                 });
             });
 
-            $("#bibdkcaptcha-controls-audiobtn").click(function() {
+            $("#bibdkcaptcha-controls-audiobtn").click(function () {
                 var type = 'wav';
 
-                var mainUrl = 'captcha/playaudiocaptcha'+"/"+$('input[name=captcha_sid]').val()+"/"+$('input[name=captcha_token]').val()+"/"
+                var mainUrl = 'captcha/playaudiocaptcha' + "/" + $('input[name=captcha_sid]').val() + "/" + $('input[name=captcha_token]').val() + "/";
                 var url = host + mainUrl;
 
-                if($.browser['msie']){
+                if ($.browser['msie']) {
                     type = 'mp3';
                 }
 
@@ -37,7 +37,7 @@
                 $("input#edit-captcha-response").focus();
             });
 
-            function updateCaptcha(data){
+            function updateCaptcha(data) {
                 var data = jQuery.parseJSON(data);
                 $("#bibdkcaptcha-controls img:first-child").remove();
                 $(data['captcha']).prependTo("#bibdkcaptcha-controls");
